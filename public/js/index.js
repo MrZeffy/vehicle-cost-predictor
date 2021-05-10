32a7f2a1a4e451c15baa757c0e163346449025e0
Daman Arora <damanarora724@gmail.com>
2021-05-10 16:54:36 +0530

    Navbar ready

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+let ourHamburgerContainer = null;
+let navbarLogo = null;
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    ourHamburger = document.getElementsByClassName('breadButton')[0];
+    navbarLogo = document.getElementById('navbarLogoImage');
+    checkWidthForBurger();
+    
+    
+    
+    window.addEventListener('resize', checkWidthForBurger);
+
+    
+});
+
+
+function checkWidthForBurger(){
+
+    if(window.innerWidth <= 500){
+        navbarLogo.src = '/images/logo-navbar-mini.png'
+    }
+
+    if (window.innerWidth < 1120) {
+
+        ourHamburger.classList.remove('hideMe');
+    } else {
+        console.log('large');
+        ourHamburger.classList.add('hideMe');
+    }
+}
